039fad03f6aa07b918a9c23b8f47b47541aa3049
Alireza Pourtaghi <alirezapt@protonmail.com>
2019-11-14 17:49:20 +0330

    Add some useful scripts.

---
+++ scripts/dockerize.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Extracting the version number from the executable file.
+VERSION=$(ls | grep -E 'kiosk-linux-.*$' | sed -E 's/kiosk-linux-v//')
+
+IMAGE_NAME=$REGISTRY_URL/$PROJECT_NAME
+case $CI_COMMIT_REF_NAME in
+	master)
+	    IMAGE_TAG=$VERSION
+		;;
+	develop)
+		IMAGE_TAG=$VERSION-INTEGRATION
+		;;
+	*)
+		IMAGE_TAG=$VERSION-DEVELOPMENT
+		;;
+esac
+
+echo About to build the $IMAGE_NAME:$IMAGE_TAG image
+docker build -t $IMAGE_NAME:$IMAGE_TAG .
+
+echo Signing into registry!
+docker login -u $REGISTRY_USER -p $REGISTRY_PASSWORD $REGISTRY_URL
+
+echo Pushing the $IMAGE_NAME:$IMAGE_TAG ...
+docker push $IMAGE_NAME:$IMAGE_TAG
+
+echo Removing the $IMAGE_NAME:$IMAGE_TAG from local build ...
+docker rmi -f $IMAGE_NAME:$IMAGE_TAG
---
+++ scripts/release.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+KIOSK_VERSION=v0.0.1
+
+go generate ./cmd/kiosk/main.go
+
+env GOOS=linux GOARCH=amd64 go build -o kiosk-linux-$KIOSK_VERSION ./cmd/kiosk
+env GOOS=freebsd GOARCH=amd64 go build -o kiosk-freebsd-$KIOSK_VERSION ./cmd/kiosk
+env GOOS=darwin GOARCH=amd64 go build -o kiosk-macos-$KIOSK_VERSION ./cmd/kiosk
+env GOOS=windows GOARCH=amd64 go build -o kiosk-windows-$KIOSK_VERSION.exe ./cmd/kiosk
