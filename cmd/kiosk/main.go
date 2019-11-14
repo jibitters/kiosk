eee018ec63df57845bb2b6be1fa80734922147f8
Alireza Pourtaghi <alirezapt@protonmail.com>
2019-11-14 20:24:43 +0330

    Some refactoring.

--- cmd/kiosk/main.go
+++ cmd/kiosk/main.go
@@ -1,5 +1,5 @@
 // Copyright 2019 The Jibit Team. All rights reserved.
-// Use of this source code is governed by a Apache-style license that can be found in the LICENSE.md file.
+// Use of this source code is governed by an Apache Style license that can be found in the LICENSE.md file.
 
 //go:generate protoc -I ../../api/protobuf-spec --go_out=plugins=grpc:../../ models.proto
 //go:generate protoc -I ../../api/protobuf-spec --go_out=plugins=grpc:../../ echo_service.proto
