53b1338d7f7c9ba9dbbc8419d6f38c0d36507e7d
Alireza Pourtaghi <alirezapt@protonmail.com>
2019-11-14 17:29:50 +0330

    Add logger implementation.

--- cmd/kiosk/main.go
+++ cmd/kiosk/main.go
@@ -1,3 +1,6 @@
+// Copyright 2019 The Jibit Team. All rights reserved.
+// Use of this source code is governed by a Apache-style license that can be found in the LICENSE.md file.
+
 //go:generate protoc -I ../../api/protobuf-spec --go_out=plugins=grpc:../../ models.proto
 //go:generate protoc -I ../../api/protobuf-spec --go_out=plugins=grpc:../../ echo_service.proto
 //go:generate protoc -I ../../api/protobuf-spec --go_out=plugins=grpc:../../ ticket_service.proto
