d61eb38dbc2689d7573ab60cffe74c32b325aee3
Alireza Pourtaghi <alirezapt@protonmail.com>
2019-11-20 13:46:28 +0330

    Add database migration tests.

--- go.mod
+++ go.mod
@@ -3,12 +3,15 @@ module github.com/jibitters/kiosk
 go 1.13
 
 require (
+	github.com/Microsoft/hcsshim v0.8.6 // indirect
+	github.com/docker/go-connections v0.4.0
 	github.com/fasthttp/router v0.5.2
 	github.com/golang-migrate/migrate/v4 v4.7.0
 	github.com/golang/protobuf v1.3.2
 	github.com/jackc/pgx/v4 v4.1.2
 	github.com/lib/pq v1.2.0
 	github.com/prometheus/client_golang v1.2.1
+	github.com/testcontainers/testcontainers-go v0.0.9
 	github.com/valyala/fasthttp v1.6.0
 	google.golang.org/grpc v1.25.1
 )
---
+++ test/containers/containers.go
@@ -0,0 +1,29 @@
+// Copyright 2019 The Jibit Team. All rights reserved.
+// Use of this source code is governed by an Apache Style license that can be found in the LICENSE.md file.
+
+package containers
+
+import (
+	"context"
+
+	"github.com/testcontainers/testcontainers-go"
+)
+
+// ContainersContext is the global context we're going to use to register containers under it.
+var ContainersContext = context.Background()
+
+// NewContainer creates a new container based on the given criteria. If we manage to successfully create and
+// start a new container, then the first returned value would be the started container. Otherwise, you should
+// checkout the error instance for more information on what exactly went wrong.
+func NewContainer(request testcontainers.ContainerRequest) (testcontainers.Container, error) {
+	return testcontainers.GenericContainer(
+		ContainersContext,
+		testcontainers.GenericContainerRequest{ContainerRequest: request, Started: true},
+	)
+}
+
+// CloseContainer stops the already started container. If we fail to stop the given container, then we would return
+// an error. Also, it's recommended to defer the call to this function.
+func CloseContainer(container testcontainers.Container) error {
+	return container.Terminate(ContainersContext)
+}
