7011f27fb67fac33bcd3a49c9e42e451dbdc60d4
Alireza Pourtaghi <alirezapt@protonmail.com>
2019-11-21 21:37:27 +0330

    Add license info to tests source files. + Add echo service tests.

---
+++ internal/app/kiosk/services/echo_service_test.go
@@ -0,0 +1,36 @@
+// Copyright 2019 The Jibit Team. All rights reserved.
+// Use of this source code is governed by an Apache Style license that can be found in the LICENSE.md file.
+
+package services
+
+import (
+	"context"
+	"testing"
+
+	rpc "github.com/jibitters/kiosk/g/rpc/kiosk"
+)
+
+func TestNewEchoService(t *testing.T) {
+	service := NewEchoService()
+
+	if service == nil {
+		t.Logf("Service initialization returned <nil> reference")
+		t.FailNow()
+	}
+}
+
+func TestEcho(t *testing.T) {
+	service := NewEchoService()
+	request := rpc.Message{Content: "echo"}
+
+	response, err := service.Echo(context.Background(), &request)
+	if err != nil {
+		t.Logf("Error : %v", err)
+		t.FailNow()
+	}
+
+	if response.Content != request.Content {
+		t.Logf("Actual : %v Expected: %v", response.Content, request.Content)
+		t.FailNow()
+	}
+}
