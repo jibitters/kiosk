9d1f80d6713b4b8b90ff175fcb0857dc03cd37f3
Alireza Pourtaghi <alirezapt@protonmail.com>
2019-11-25 09:54:53 +0330

    Update test script.

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,7 +5,7 @@ go generate ./cmd/kiosk/main.go
 set -e
 echo "" > coverage.txt
 
-for d in $(go list ./cmd/... ./internal/... ./test/...); do
+for d in $(go list ./...); do
     go test -coverprofile=profile.out -failfast "$d"
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
