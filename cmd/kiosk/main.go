bbc13dcf368073330688f6c2552dadba67bc064d
Alireza Pourtaghi <alirezapt@protonmail.com>
2020-06-15 16:09:51 +0430

    Setup basic project layout.

---
+++ cmd/kiosk/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"flag"
+	"os"
+	"os/signal"
+
+	"github.com/lireza/lib/configuring"
+	"go.uber.org/zap"
+)
+
+var config = flag.String("config", "./configs/kiosk.json", "configuration file")
+
+// Kiosk is the main program encapsulation that holds all required components.
+type Kiosk struct {
+	logger *zap.SugaredLogger
+	config *configuring.Config
+}
+
+func main() {
+	kiosk := setup()
+
+	kiosk.configure()
+	kiosk.awaitTermination()
+}
+
+func setup() *Kiosk {
+	flag.Parse()
+	logger, _ := zap.NewDevelopment()
+	config := configuring.New()
+
+	return &Kiosk{logger: logger.Sugar(), config: config}
+}
+
+func (k *Kiosk) configure() {
+	k.logger.Info("Loading configuration file from ", *config)
+	if _, e := k.config.LoadJSON(*config); e != nil {
+		k.logger.Fatal(e.Error())
+	}
+
+	environment := k.config.Get("logger.environment").StringOrElse("DEVELOPMENT")
+	k.logger.Debug("logger.environment -> ", environment)
+
+	if environment == "PRODUCTION" {
+		logger, _ := zap.NewProduction()
+		k.logger = logger.Sugar()
+	}
+}
+
+func (k *Kiosk) awaitTermination() {
+	receiver := make(chan os.Signal)
+	signal.Notify(receiver, os.Interrupt)
+
+	<-receiver
+	k.logger.Debug("Received interrupt signal!")
+
+	k.stop()
+}
+
+func (k *Kiosk) stop() {
+	k.logger.Info("Stopping the process ...")
+	_ = k.logger.Sync()
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/jibitters/kiosk
 
 go 1.14
+
+require (
+	github.com/lireza/lib v0.0.13
+	go.uber.org/zap v1.15.0
+)
