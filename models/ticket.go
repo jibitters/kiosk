bac2c044efac975e5d650ca7b6e81caf447e9f65
Alireza Pourtaghi <alirezapt@protonmail.com>
2020-06-16 23:07:04 +0430

    Add ticket repository related methods.

--- models/ticket.go
+++ models/ticket.go
@@ -1,7 +1,12 @@
 package models
 
 import (
+	"context"
+	"database/sql"
+
+	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/jibitters/kiosk/errors"
 	"go.uber.org/zap"
 )
 
@@ -29,7 +34,119 @@ func NewTicketRepository(logger *zap.SugaredLogger, db *pgxpool.Pool) *TicketRep
 	return &TicketRepository{logger: logger, db: db}
 }
 
-// ImportanceLevel model.
+// Insert tries to insert a ticket into tickets table.
+func (r *TicketRepository) Insert(ctx context.Context, ticket Ticket) *errors.Type {
+	q := `INSERT INTO tickets (
+			issuer,
+			owner,
+			subject,
+			content,
+			metadata,
+			importance_level,
+			status,
+			created_at,
+			modified_at) VALUES ($1, $2, $3, $4, $5, $6, $7, NOW(), NOW());`
+
+	_, e := r.db.Exec(ctx, q,
+		ticket.Issuer,
+		ticket.Owner,
+		ticket.Subject,
+		ticket.Content,
+		ticket.Metadata,
+		ticket.ImportanceLevel,
+		TicketStatusNew,
+	)
+
+	if e != nil {
+		et := errors.InternalServerError("unknown", "")
+		r.logger.Error(et.FingerPrint, ": ", e.Error())
+		return et
+	}
+
+	return nil
+}
+
+// LoadByID tries to load a ticket from tickets table.
+func (r *TicketRepository) LoadByID(ctx context.Context, id int64) (*Ticket, *errors.Type) {
+	q := `SELECT
+			id,
+			issuer,
+			owner,
+			subject,
+			content,
+			metadata,
+			importance_level,
+			status,
+			created_at,
+			modified_at FROM tickets WHERE id = $1;`
+
+	ticket := &Ticket{}
+	var metadata sql.NullString
+
+	row := r.db.QueryRow(ctx, q, id)
+	e := row.Scan(
+		&ticket.ID,
+		&ticket.Issuer,
+		&ticket.Owner,
+		&ticket.Subject,
+		&ticket.Content,
+		&metadata,
+		&ticket.ImportanceLevel,
+		&ticket.Status,
+		&ticket.CreatedAt,
+		&ticket.ModifiedAt,
+	)
+
+	if e != nil {
+		if e == pgx.ErrNoRows {
+			return nil, errors.NotFound("ticket.not_found", "")
+		}
+
+		et := errors.InternalServerError("unknown", "")
+		r.logger.Error(et.FingerPrint, ": ", e.Error())
+		return nil, et
+	}
+
+	if metadata.Valid {
+		ticket.Metadata = metadata.String
+	}
+
+	return ticket, nil
+}
+
+// Update tries to update a ticket record.
+func (r *TicketRepository) Update(ctx context.Context, ticket *Ticket) *errors.Type {
+	q := `UPDATE tickets
+			SET subject = $1,
+				metadata = $2,
+				importance_level = $3,
+				status = $4,
+				modified_at = NOW()
+			WHERE id = $5;`
+
+	command, e := r.db.Exec(ctx, q,
+		ticket.Subject,
+		ticket.Metadata,
+		ticket.ImportanceLevel,
+		ticket.Status,
+		ticket.ID,
+	)
+
+	if e != nil {
+		et := errors.InternalServerError("unknown", "")
+		r.logger.Error(et.FingerPrint, ": ", e.Error())
+		return et
+	}
+
+	if command.RowsAffected() == 0 {
+		et := errors.PreconditionFailed("ticket.not_found", "")
+		return et
+	}
+
+	return nil
+}
+
+// TicketImportanceLevel model.
 type TicketImportanceLevel string
 
 // Different importance level instances.
---
+++ test/helper.go
@@ -0,0 +1,83 @@
+package test
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/jibitters/kiosk/db/postgres"
+	"github.com/lireza/lib/configuring"
+	"go.uber.org/zap"
+)
+
+// ConnectToDatabase connects to a postgres instance listening on port and then runs database migration.
+func ConnectToDatabase(host string, port int) (*pgxpool.Pool, error) {
+	config := configuring.New()
+
+	directory, e := ioutil.TempDir("", "migration")
+	if e != nil {
+		return nil, e
+	}
+
+	file, e := ioutil.TempFile(directory, "1_*.up.sql")
+	if e != nil {
+		return nil, e
+	}
+
+	defer func() { _ = file.Close() }()
+
+	_, _ = file.WriteString(first)
+	_ = os.Setenv("DB_POSTGRES_CONNECTION_STRING",
+		fmt.Sprintf("postgres://user:password@%v:%v/kiosk?sslmode=disable", host, port))
+
+	_ = os.Setenv("DB_POSTGRES_MIGRATION_DIRECTORY",
+		"file://"+filepath.Dir(file.Name()))
+
+	if e := postgres.Migrate(zap.S(), config); e != nil {
+		return nil, e
+	}
+
+	db, e := postgres.Connect(zap.S(), config)
+	if e != nil {
+		return nil, e
+	}
+
+	return db, nil
+}
+
+var first = `
+-- Tickets table definition.
+CREATE TABLE tickets
+(
+    id               BIGSERIAL    NOT NULL,
+    issuer           VARCHAR(50)  NOT NULL,
+    owner            VARCHAR(50)  NOT NULL,
+    subject          VARCHAR(255) NOT NULL,
+    content          TEXT         NOT NULL,
+    metadata         TEXT,
+    importance_level VARCHAR(25)  NOT NULL,
+    status           VARCHAR(25)  NOT NULL,
+    created_at       TIMESTAMP    NOT NULL,
+    modified_at      TIMESTAMP    NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE INDEX tickets_owner_importance_level_status_modified_at ON tickets (owner, importance_level, status, modified_at);
+
+-- Comments table definition.
+CREATE TABLE comments
+(
+    id          BIGSERIAL   NOT NULL,
+    ticket_id   BIGINT REFERENCES tickets,
+    owner       VARCHAR(50) NOT NULL,
+    content     TEXT        NOT NULL,
+    metadata    TEXT,
+    created_at  TIMESTAMP   NOT NULL,
+    modified_at TIMESTAMP   NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE INDEX comments_ticket_id_created_at ON comments (ticket_id, created_at);
+`
