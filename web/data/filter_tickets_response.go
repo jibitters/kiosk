6fe3b71336730d8509fe6c929001fd29a0889a0d
Alireza Pourtaghi <alirezapt@protonmail.com>
2020-06-17 22:43:15 +0430

    Add filter tickets implementation.

--- web/data/filter_tickets_response.go
+++ web/data/filter_tickets_response.go
@@ -1,7 +1,22 @@
 package data
 
+import (
+	"github.com/jibitters/kiosk/models"
+)
+
 // FilterTicketsResponse model definition.
 type FilterTicketsResponse struct {
-	Tickets     []TicketResponse `json:"tickets"`
-	HasNextPage bool             `json:"hasNextPage"`
+	Tickets     []*TicketResponse `json:"tickets"`
+	HasNextPage bool              `json:"hasNextPage"`
+}
+
+// LoadFromTickets loads the fields of current model from provided tickets.
+func (ftr *FilterTicketsResponse) LoadFromTickets(tickets []*models.Ticket, HasNextPage bool) {
+	for _, t := range tickets {
+		ticketResponse := &TicketResponse{}
+		ticketResponse.LoadFromTicket(t)
+		ftr.Tickets = append(ftr.Tickets, ticketResponse)
+	}
+
+	ftr.HasNextPage = HasNextPage
 }
